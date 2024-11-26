c89086162bc1ec3d112fe463d5f99f458c931719
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-11-26 14:58:56 +0000

    Add a search endpoint

---
+++ SwanSong.Domain/Dto/Search.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace SwanSong.Domain.Dto;
+public class Search
+{
+    public record SearchResponse(List<AlbumLookUpResponse> albums, List<ArtistLookUpResponse> artists, List<MemberLookUpResponse> members);
+}
